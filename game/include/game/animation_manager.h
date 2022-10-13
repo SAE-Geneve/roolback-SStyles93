2cdf633d09bb7e30c92ba3b0c2fff4a4000da147
SStyles93 <samuel.styles93@gmail.com>
2022-10-13 22:52:31 +0200

    Untracked Files Added
    AnimationManager(.h & .cpp)
    Sprites (idle, jump, shoot, walk)

---
+++ game/include/game/animation_manager.h
@@ -0,0 +1,62 @@
+#pragma once
+#include <filesystem>
+#include <SFML/System/Time.hpp>
+
+#include "game_globals.h"
+#include "SFML/Graphics/Texture.hpp"
+
+
+namespace core
+{
+	class SpriteManager;
+}
+
+namespace game
+{
+class GameManager;
+
+enum class AnimationState
+{
+    IDLE,
+    WALK,
+    JUMP,
+    SHOOT,
+    NONE
+};
+struct Animation
+{
+    int textureIdx = 0;
+    std::vector<sf::Texture> textures{};
+    AnimationState  animationState = AnimationState::NONE;
+};
+/**
+ * \brief AnimationManager is a ComponentManager that holds all the animations in one place.
+ */
+class AnimationManager : public core::ComponentManager<Animation, static_cast<core::EntityMask>(ComponentType::ANIMATION)>
+{
+
+public:
+
+    AnimationManager(core::EntityManager& entityManager, core::SpriteManager& spriteManager, GameManager& gameManager);
+
+    //void LoadTexture(std::string path, std::vector<sf::Texture>& textures) const;
+    void LoadTexture(std::string_view path, Animation& animation) const;
+    //void PlayAnimation(core::Entity& entity, const std::vector<sf::Texture>& textures, float speed);
+    void PlayAnimation(const core::Entity& entity, Animation& animation, float speed);
+	//void UpdateEntity(sf::Time dt);
+	void UpdateEntity(core::Entity entity, AnimationState animationState, sf::Time dt);
+
+    float animationTime_ = 0;
+
+    Animation catIdle;
+    Animation catWalk;
+	Animation catJump;
+	Animation catShoot;
+
+private:
+
+	core::SpriteManager& spriteManager_;
+    GameManager& gameManager_;
+};
+}
+
---
+++ game/src/game/animation_manager.cpp
@@ -0,0 +1,194 @@
+#include "game/animation_manager.h"
+#include "game/game_manager.h"
+#include <fmt/format.h>
+
+namespace game
+{
+AnimationManager::AnimationManager(core::EntityManager& entityManager, core::SpriteManager& spriteManager, GameManager& gameManager) :
+	ComponentManager(entityManager), spriteManager_(spriteManager), gameManager_(gameManager)
+{
+	catIdle.animationState = AnimationState::IDLE;
+	catWalk.animationState = AnimationState::WALK;
+	catJump.animationState = AnimationState::JUMP;
+	catShoot.animationState = AnimationState::SHOOT;
+}
+
+void AnimationManager::LoadTexture(const std::string_view path, Animation& animation) const
+{
+	auto format = fmt::format("data/sprites/{}", path);
+	auto dirIter = std::filesystem::directory_iterator(fmt::format("data/sprites/{}",path));
+	const int textureCount = std::count_if(
+		begin(dirIter),
+		end(dirIter),
+		[](auto& entry) { return entry.is_regular_file() && entry.path().extension() == ".png"; });
+
+	//LOAD SPRITES
+	for (size_t i = 0; i < textureCount; i++)
+	{
+		sf::Texture newTexture;
+		const auto fullPath = fmt::format("data/sprites/{}/{}{}.png",path, path, i);
+		if (!newTexture.loadFromFile(fullPath))
+		{
+			core::LogError(fmt::format("Could not load data/sprites/{}/{}{}.png sprite",path, path, i));
+		}
+		animation.textures.push_back(newTexture);
+	}
+}
+
+void AnimationManager::PlayAnimation(const core::Entity& entity,Animation& animation, float speed)
+{
+	auto& playerCharacter = gameManager_.GetRollbackManager().GetPlayerCharacterManager().GetComponent(entity);
+	auto& playerSprite = spriteManager_.GetComponent(entity);
+	
+	
+	switch (playerCharacter.animationState)
+	{
+	case AnimationState::IDLE:
+	case AnimationState::WALK:
+		//Ignores animations if the "cat_shoot" animation is playing
+		if(playerCharacter.isShooting) 
+			break;
+		//Plays animation
+		if (animationTime_ >= ANIMATION_PERIOD / speed)
+		{
+			animation.textureIdx++;
+			if (animation.textureIdx >= animation.textures.size())
+			{
+				//resets the animation (it loops)
+				animation.textureIdx = 0;
+			}
+			animationTime_ = 0;
+		}
+		//Prevents texture index from being out of range of the textures vector
+		if (animation.textureIdx >= animation.textures.size())
+		{
+			animation.textureIdx = animation.textures.size() - 1;
+		}
+		playerSprite.setTexture(animation.textures[animation.textureIdx]);
+		break;
+
+	case AnimationState::JUMP:
+		if (playerCharacter.isShooting)
+			break;
+
+		if (playerCharacter.isGrounded)
+		{
+			animation.textureIdx = 0;
+		}
+		if (animationTime_ >= ANIMATION_PERIOD / speed)
+		{
+			animation.textureIdx++;
+			if (animation.textureIdx >= animation.textures.size())
+			{
+				//blocks the animation on last texture
+				animation.textureIdx = animation.textures.size() - 1;
+			}
+			animationTime_ = 0;
+		}
+		if (animation.textureIdx >= animation.textures.size())
+		{
+			animation.textureIdx = animation.textures.size() - 1;
+		}
+		playerSprite.setTexture(animation.textures[animation.textureIdx]);
+		break;
+
+	case AnimationState::SHOOT:
+		if (animationTime_ >= ANIMATION_PERIOD / speed)
+		{
+			animation.textureIdx++;
+			if (animation.textureIdx >= animation.textures.size())
+			{
+				if(!playerCharacter.isShooting)
+				{
+					animation.textureIdx = 0;
+				}else
+				{
+					animation.textureIdx = animation.textures.size() - 1;
+				}
+			}
+			animationTime_ = 0;
+		}
+		if (animation.textureIdx >= animation.textures.size())
+		{
+			animation.textureIdx = animation.textures.size() - 1;
+		}
+		playerSprite.setTexture(animation.textures[animation.textureIdx]);
+		break;
+
+	case AnimationState::NONE:
+		core::LogError("PlayAnimation trying to play \"NONE\"");
+		break;
+
+	default:
+		core::LogError("PlayAnimation not working as expected");
+		break;
+	}
+}
+
+//void AnimationManager::UpdateEntity(sf::Time dt)
+//{
+//	animationTime_ += dt.asSeconds();
+//
+//	/*switch (player.animationState)
+//	{
+//	case AnimationState::IDLE:
+//		PlayAnimation(entity, catIdleTextures_, 1.0f);
+//		break;
+//
+//	case AnimationState::WALK:
+//		PlayAnimation(entity, catWalkTextures_, 1.0f);
+//		break;
+//
+//	case AnimationState::JUMP:
+//		PlayAnimation(entity, catJumpTextures_, 2.0f);
+//		break;
+//
+//	case AnimationState::NONE:
+//
+//		core::LogError("Animation state \"NONE\"");
+//		break;
+//
+//	default:
+//
+//		core::LogError("AnimationState Default, not supposed to happen !");
+//		break;
+//	}*/
+//}
+
+void AnimationManager::UpdateEntity(core::Entity entity, AnimationState animationState, sf::Time dt)
+{
+	animationTime_ += dt.asSeconds();
+	switch (animationState)
+	{
+	case AnimationState::IDLE:
+		PlayAnimation(entity, catIdle, 1.0f);
+		break;
+
+	case AnimationState::WALK:
+		PlayAnimation(entity, catWalk, 1.0f);
+		break;
+
+	case AnimationState::JUMP:
+		PlayAnimation(entity, catJump, 2.0f);
+		break;
+	case AnimationState::SHOOT:
+		PlayAnimation(entity, catShoot, 1.0f);
+		break;
+
+	case AnimationState::NONE:
+
+		core::LogError("Animation state \"NONE\"");
+		break;
+
+	default:
+		core::LogError("AnimationState Default, not supposed to happen !");
+		break;
+	}
+}
+
+}
+
+
+
+
+
